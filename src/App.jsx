6995e5637792f68d1ab4cf50490c14c45bdfbe84
shivibhadauria <shivi30bhadauria@gmail.com>
2024-09-22 23:23:45 +0530

    correct routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Main from './components/main';
+
+
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Main />} />
-        {/* Add more routes here if needed */}
+        {/* Define your routes here, for example: */}
+        <Route path="./" element={<Main />} />
+        {/* Add more routes as needed */}
       </Routes>
     </Router>
   );
 }
+
+
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: "/cv_portfolio/",
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
-    },
-  },
 });
